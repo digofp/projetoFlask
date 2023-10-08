0f5891f947c049d62b0e8a2f55325f56ae98ccf8
Rodrigo Perez <digofp@gmail.com>
2023-10-08 20:54:45 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    parameter = "Texto"
+    return render_template("index.html", parameter=parameter)
+
+@app.route('/contato')
+def contato():
+    data = {"profissao": "Analista", "nome": "Rodrigo"}
+    return render_template("contato.html", data=data)
